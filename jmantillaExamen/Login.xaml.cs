daca4ca353fc238c955c92717860c6392ff50a8b
master <master@SMLAB408PC21_23>
2023-01-10 19:46:45 -0500

    Agregar archivos de proyecto.

---
+++ jmantillaExamen/Login.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace jmantillaExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "estudiante2023";
+            string contraseña = "uisrael2023";
+            if (usuario == txtUsuario.Text && contraseña == txtContraseña.Text)
+            {
+                DisplayAlert("Bienvenido", usuario, "Cerrar");
+                Navigation.PushAsync(new Registro(usuario));
+            }
+            else
+            {
+                DisplayAlert("ALERTA", "Uusuario/Contraseña incorrecto", "Cerrar");
+            }
+
+        }
+    
+    }
+}
---
+++ jmantillaExamen/Registro.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace jmantillaExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(string usuario)
+        {
+            InitializeComponent();
+            lblUsuario.Text = usuario;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            //double datoSeguimientoUno = Convert.ToDouble(txtDatoSeguimientoUno.Text) * 0.3;
+            double pagoAdicional = 4000 * 0.05;
+            double pagoInicial= Convert.ToDouble(txtMontoInicial.Text);
+            double pagoMensual = ((4000 - pagoInicial) / 5) + pagoAdicional;
+            string nombre = txtNombre.Text;
+            lblPagoMensual.Text = Convert.ToString(pagoMensual);
+
+            
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            string usuario = lblUsuario.Text;
+            double pagoAdicional = 4000 * 0.05;
+            string nombre = txtNombre.Text;
+            double pagoInicial = Convert.ToDouble(txtMontoInicial.Text);
+            double pagoMensual = ((4000 - pagoInicial) / 5) + pagoAdicional;
+            DisplayAlert("Guardado con Exito", nombre, "Cerrar");
+            Navigation.PushAsync(new Resumen(nombre, pagoInicial, pagoMensual, usuario));
+        }
+    }
+}
